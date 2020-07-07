5d2a4cb61ac63570ffe0c96429b3c21b01c76ae0
eric <eric.jeangirard@recherche.gouv.fr>
2020-07-07 17:12:58 +0200

    update

--- docker_build.sh
+++ docker_build.sh
@@ -1,7 +1,7 @@
-#sudo docker build -f Dockerfile_es -t dataesr/es_icu -t dataesr/es_icu:1.0 .
-#sudo docker push dataesr/es_icu
+sudo docker build -f Dockerfile_es -t dataesr/es_icu -t dataesr/es_icu:1.0 .
+sudo docker push dataesr/es_icu
 
-sudo docker build -f Dockerfile -t dataesr/matcher -t dataesr/matcher:1.0 .
-sudo docker push dataesr/matcher
+#sudo docker build -f Dockerfile -t dataesr/matcher -t dataesr/matcher:1.1 .
+#sudo docker push dataesr/matcher
 
-docker-compose down && docker-compose pull && docker-compose up
+#docker-compose down && docker-compose pull && docker-compose up
