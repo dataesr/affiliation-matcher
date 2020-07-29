5999b5d8f772d225df9cfc2eb877304feed8d06e
eric <eric.jeangirard@recherche.gouv.fr>
2020-07-29 11:17:40 +0200

    bug fixes and tests

--- project/server/main/local_test.py
+++
@@ -1,4 +0,0 @@
-from fake_test import *
-import sys
-
-crawl(sys.argv[1], sys.argv[2])
---
+++ test_match.py
@@ -0,0 +1,17 @@
+from match_rnsr import match_unstructured, match_fields
+
+def test_1():
+    res = match_unstructured(2015, "INS1640 INSHS Institut des Sciences Humaines et Sociales ORLEANS")
+    assert res.get('match') == None
+
+def test_2():
+    res = match_unstructured(2015, "MOY1000  Délégation Alsace STRASBOURG")
+    assert res.get('match') == None
+
+def test_3():
+    res = match_fields(2018, "UMR 5223", "INGENIERIE DES MATERIAUX POLYMERES", "VILLEURBANNE CEDEX", "IMP", "196917744")
+    assert res.get('match') == '200711890Y'
+
+def test_4():
+    res = match_fields(2019, None, "BUREAU DE RECHERCHES GEOLOGIQUES ET MINIERES", "ORLEANS", "BRGM", "582056149")
+    assert res.get('match') == '195922846R'
