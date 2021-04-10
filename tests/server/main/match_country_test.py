8ab51a04cea8f87299861d56a08f72fe71c28e66
Anne L'Hôte <anne.lhote@gmail.com>
2021-04-10 18:43:19 +0200

    [#5][clean] Clean files

--- tests/server/main/match_country_test.py
+++ tests/server/main/match_country_test.py
@@ -4,7 +4,8 @@ from project.server.main.match_country import get_address_from_query
 
 
 @pytest.mark.parametrize('query,country_iso_3', [
-    ('Medical Intensive Care Unit, Hôpital Lyon Sud, Pierre-Bénite 69495, France, COVID-O-HCL Consortium, Hospices Civils de Lyon, Lyon, France.', 'FRA')
+    ('Medical Intensive Care Unit, Hôpital Lyon Sud, Pierre-Bénite 69495, France, COVID-O-HCL Consortium, Hospices '
+     'Civils de Lyon, Lyon, France.', 'FRA')
 ])
 def test_get_address_from_query(query, country_iso_3) -> None:
     response = get_address_from_query(query)
