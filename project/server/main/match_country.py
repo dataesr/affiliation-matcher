bd5b4039b33202eb999f62a6622c97c8f2bd92f3
Anne L'Hôte <anne.lhote@gmail.com>
2021-04-10 18:24:14 +0200

    [#5] Plug web interface for country matcher

--- project/server/main/match_country.py
+++ project/server/main/match_country.py
@@ -6,4 +6,7 @@ from project.server.main.config import config
 def get_address_from_query(query) -> dict:
     geocoder = OpenCageGeocode(config['OPENCAGE_API_KEY'])
     results = geocoder.geocode(query)
-    return results[0]
+    logs = 'We found {} result(s):'.format(len(results))
+    logs += '<br>'
+    logs += results[0].get('components', None).get('ISO_3166-1_alpha-3', None)
+    return {'match': results, 'logs': logs}
