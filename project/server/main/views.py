164fbcec220b69d9f061b312f8580f32918a3ace
Anne L'Hôte <anne.lhote@gmail.com>
2021-04-12 14:47:56 +0200

    [clean][PEP8] Reformat files

--- project/server/main/views.py
+++ project/server/main/views.py
@@ -1,32 +1,37 @@
 import redis
+
 from rq import Queue, Connection
 from flask import render_template, Blueprint, jsonify, request, current_app
 
-from project.server.main.tasks import create_task_match, create_task_rnsr, create_task_init_rnsr, create_task_init_finess
+from project.server.main.tasks import create_task_match, create_task_rnsr, create_task_init_rnsr, \
+    create_task_init_finess
 
-main_blueprint = Blueprint("main", __name__,)
+main_blueprint = Blueprint("main", __name__, )
 
 
 @main_blueprint.route("/", methods=["GET"])
 def home():
     return render_template("home.html")
 
+
 @main_blueprint.route("/init", methods=["GET"])
 def run_task_init_rnsr():
     response_object = create_task_init_rnsr()
     return jsonify(response_object), 202
+
 
 @main_blueprint.route("/init_finess", methods=["GET"])
 def run_task_init_finess():
     response_object = create_task_init_finess()
     return jsonify(response_object), 202
 
+
 @main_blueprint.route("/match_api", methods=["POST"])
 def run_task_match():
     args = request.get_json(force=True)
     print(args, flush=True)
     response_object = create_task_match(args)
-    
+
     return jsonify(response_object), 202
 
 
@@ -35,19 +40,18 @@ def run_task_rnsr():
     args = request.get_json(force=True)
     print(args, flush=True)
     response_object = create_task_rnsr(args)
-    #my_arguments = {'url': url, 'title': title}
-    #print(my_arguments, flush=True)
-    
-    
-    #with Connection(redis.from_url(current_app.config["REDIS_URL"])):
+    # my_arguments = {'url': url, 'title': title}
+    # print(my_arguments, flush=True)
+
+    # with Connection(redis.from_url(current_app.config["REDIS_URL"])):
     #    q = Queue(default_timeout=21600)
     #    task = q.enqueue(create_task_harvest, args)
-    #response_object = {
+    # response_object = {
     #    "status": "success",
     #    "data": {
     #        "task_id": task.get_id()
     #    }
-    #}
+    # }
     return jsonify(response_object), 202
 
 
