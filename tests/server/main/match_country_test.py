59befeb6ee61416861e4b358806edcbfdf6cd8ad
Anne L'Hôte <anne.lhote@gmail.com>
2021-04-12 17:06:10 +0200

    [#5] CHange geocoder from OpenCage to Normatim (OSM) via geopy

--- tests/server/main/match_country_test.py
+++ tests/server/main/match_country_test.py
@@ -4,8 +4,7 @@ from project.server.main.match_country import get_address_from_query
 
 
 @pytest.mark.parametrize('query,country_iso_3', [
-    ('Medical Intensive Care Unit, Hôpital Lyon Sud, Pierre-Bénite 69495, France, COVID-O-HCL Consortium, Hospices '
-     'Civils de Lyon, Lyon, France.', 'FR')
+    ('Tour mirabeau paris', 'fr')
 ])
 def test_get_address_from_query(query, country_iso_3) -> None:
     response = get_address_from_query(query)
