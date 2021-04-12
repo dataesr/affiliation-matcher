52163f6874c0077f251c6e39bdf0d466f49742bf
Anne L'Hôte <anne.lhote@gmail.com>
2021-04-12 14:54:28 +0200

    [#2][#5] Add logger in country matcher

--- project/server/main/match_country.py
+++ project/server/main/match_country.py
@@ -1,12 +1,18 @@
+import logging
+
 from opencage.geocoder import OpenCageGeocode
 
 from project.server.main.config import config
 
 
 def get_address_from_query(query) -> dict:
+    logging.basicConfig(filename='matcher.log', level=logging.DEBUG,
+                        format='%(asctime)s | %(name)s | %(levelname)s | %(message)s')
     geocoder = OpenCageGeocode(config['OPENCAGE_API_KEY'])
     results = geocoder.geocode(query)
-    logs = 'We found {} result(s):'.format(len(results))
-    logs += '<br>'
-    logs += results[0].get('components', None).get('ISO_3166-1_alpha-3', None)
-    return {'match': results, 'logs': logs}
+    if len(results) == 0:
+        logging.error('No country found for {}'.format(query))
+        country = None
+    else:
+        country = results[0].get('components', None).get('ISO_3166-1_alpha-2', None)
+    return {'match': country}
