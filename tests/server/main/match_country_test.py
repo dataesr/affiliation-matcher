51c08864d3f688878bb67b2868ea8f0943f79ea5
Anne L'Hôte <anne.lhote@gmail.com>
2021-04-12 14:54:28 +0200

    [#2][#5] Add logger in country matcher

--- tests/server/main/match_country_test.py
+++ tests/server/main/match_country_test.py
@@ -5,9 +5,8 @@ from project.server.main.match_country import get_address_from_query
 
 @pytest.mark.parametrize('query,country_iso_3', [
     ('Medical Intensive Care Unit, Hôpital Lyon Sud, Pierre-Bénite 69495, France, COVID-O-HCL Consortium, Hospices '
-     'Civils de Lyon, Lyon, France.', 'FRA')
+     'Civils de Lyon, Lyon, France.', 'FR')
 ])
 def test_get_address_from_query(query, country_iso_3) -> None:
     response = get_address_from_query(query)
-    first_result = response.get('match', None)[0]
-    assert first_result.get('components', None).get('ISO_3166-1_alpha-3', None) == country_iso_3
+    assert response.get('match', None) == country_iso_3
