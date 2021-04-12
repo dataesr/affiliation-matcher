30f4abf46ad7a7a290b5b146574743ee018f59d2
Anne L'Hôte <anne.lhote@gmail.com>
2021-04-12 17:06:10 +0200

    [#5] CHange geocoder from OpenCage to Normatim (OSM) via geopy

--- project/server/main/match_country.py
+++ project/server/main/match_country.py
@@ -1,18 +1,19 @@
 import logging
 
-from opencage.geocoder import OpenCageGeocode
-
-from project.server.main.config import config
+from geopy.geocoders import Nominatim
 
 
 def get_address_from_query(query) -> dict:
     logging.basicConfig(filename='matcher.log', level=logging.DEBUG,
                         format='%(asctime)s | %(name)s | %(levelname)s | %(message)s')
-    geocoder = OpenCageGeocode(config['OPENCAGE_API_KEY'])
-    results = geocoder.geocode(query)
-    if len(results) == 0:
+    geolocator = Nominatim(user_agent="another_user_agent")
+    geocode = geolocator.geocode(query)
+    if geocode is None:
         logging.error('No country found for {}'.format(query))
         country = None
     else:
-        country = results[0].get('components', None).get('ISO_3166-1_alpha-2', None)
+        lat = geocode.raw.get('lat', None)
+        lon = geocode.raw.get('lon', None)
+        location = geolocator.reverse('{}, {}'.format(lat, lon))
+        country = location.raw.get('address', None).get('country_code', None)
     return {'match': country}
