570233f48fc0a0d68303856b8e64743d7a099f51
Anne L'Hôte <anne.lhote@gmail.com>
2021-04-13 19:32:55 +0200

    [#5] Add recurrence for the country matcher

--- tests/server/main/match_country_test.py
+++ tests/server/main/match_country_test.py
@@ -1,11 +1,15 @@
 import pytest
 
-from project.server.main.match_country import get_address_from_query
+from project.server.main.match_country import get_country_from_query
 
 
 @pytest.mark.parametrize('query,expected_country', [
-    ('Tour mirabeau paris', 'fr')
+    ('Tour Mirabeau Paris', 'fr'),
+    ('Inserm U1190 European Genomic Institute of Diabetes, CHU Lille, Lille, France', 'fr'),
+    ('Inserm U1190 European Genomic Institute of Diabetes CHU Lille Lille France', None),
+    ('Sorbonne Université, INSERM, UMRS 1142 LIMICS, Paris, France, APHP.Sorbonne, Fetal Medicine Department, '
+     'Armand Trousseau Hospital, Paris, France.', 'fr')
 ])
 def test_get_address_from_query(query, expected_country) -> None:
-    matched_country = get_address_from_query(query)
+    matched_country = get_country_from_query(query)
     assert matched_country == expected_country
