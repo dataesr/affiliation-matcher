080983d1677d243fea967930b868f19d0432280a
Anne L'Hôte <anne.lhote@gmail.com>
2021-04-23 17:46:57 +0200

    [#5] Add country matcher

--- tests/server/main/match_country_test.py
+++ tests/server/main/match_country_test.py
@@ -1,15 +1,15 @@
 import pytest
 
-from matcher.server.main.match_country import get_country_from_query
+from matcher.server.main.match_country import get_countries_from_query
 
 
 @pytest.mark.parametrize('query,expected_country', [
-    ('Tour Mirabeau Paris', 'fr'),
-    ('Inserm U1190 European Genomic Institute of Diabetes, CHU Lille, Lille, France', 'fr'),
-    ('Inserm U1190 European Genomic Institute of Diabetes CHU Lille Lille France', None),
+    ('Tour Mirabeau Paris', ['FR']),
+    ('Inserm U1190 European Genomic Institute of Diabetes, CHU Lille, Lille, France', ['FR']),
+    ('N\'importe quoi', []),
     ('Sorbonne Université, INSERM, UMRS 1142 LIMICS, Paris, France, APHP.Sorbonne, Fetal Medicine Department, '
-     'Armand Trousseau Hospital, Paris, France.', 'fr')
+     'Armand Trousseau Hospital, Paris, France.', ['FR'])
 ])
 def test_get_address_from_query(query, expected_country) -> None:
-    matched_country = get_country_from_query(query)
+    matched_country = get_countries_from_query(query)
     assert matched_country == expected_country
