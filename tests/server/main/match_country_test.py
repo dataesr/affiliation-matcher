29d3830058c6a9e6f878031f1721d4dfcbbe9038
Anne L'Hôte <anne.lhote@gmail.com>
2021-04-28 10:05:49 +0200

    [#7] Add universities on match_country step

--- tests/server/main/match_country_test.py
+++ tests/server/main/match_country_test.py
@@ -17,10 +17,13 @@ def elasticsearch() -> dict:
 
 class TestMatchCountry:
     @pytest.mark.parametrize('fields,values,is_complex,expected_regex', [
-        (['cities'], [['city_01']], True, re.compile('(?<![a-z])city_01(?![a-z])', re.IGNORECASE)),
-        (['cities'], [['city_01', 'city_02']], True, re.compile('(?<![a-z])city_01(?![a-z])|(?<![a-z])city_02(?![a-z])', re.IGNORECASE)),
-        (['cities', 'info'], [['city_01'], ['info_01']], True, re.compile('(?<![a-z])city_01(?![a-z])|(?<![a-z])info_01(?![a-z])', re.IGNORECASE)),
-        (['stop_words'], [['word_01', 'word_02']], False, re.compile('word_01|word_02', re.IGNORECASE))
+        (['cities'], [['city_01']], True, re.compile('(?<![a-z])city 01(?![a-z])', re.IGNORECASE)),
+        (['cities'], [['city_01', 'city_02']], True, re.compile('(?<![a-z])city 01(?![a-z])|(?<![a-z])city 02(?![a-z])',
+                                                                re.IGNORECASE)),
+        (['cities', 'info'], [['city_01'], ['info_01']], True,
+         re.compile('(?<![a-z])city 01(?![a-z])|(?<![a-z])info 01(?![a-z])', re.IGNORECASE)),
+        (['universities'], [['université']], True, re.compile('(?<![a-z])universite(?![a-z])', re.IGNORECASE)),
+        (['stop_words'], [['word_01', 'word_02']], False, re.compile('word 01|word 02', re.IGNORECASE))
     ])
     def test_get_regex_from_country_by_fields(self, elasticsearch, fields, values, is_complex, expected_regex) -> None:
         body = {}
@@ -39,7 +42,8 @@ class TestMatchCountry:
             ('Sorbonne Université, INSERM, UMRS 1142 LIMICS, Paris, France, APHP.Sorbonne, Fetal Medicine Department, '
              'Armand Trousseau Hospital, Paris, France.', ['fr']),
             ('Hotel-Dieu de France University Hospital, Faculty of Medicine, Saint Joseph University, Beirut, Lebanon.',
-             ['lb'])
+             ['lb']),
+            ('Université de technologie de Troyes', ['fr'])
         ])
     def test_get_countries_from_query(self, query, expected_country) -> None:
         matched_country = get_countries_from_query(query)
