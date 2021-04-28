3c942bcd71baf4c8e9a88ebb9ba9bda81711f3d8
Anne L'Hôte <anne.lhote@gmail.com>
2021-04-28 08:15:22 +0200

    [#7] Add French universities on init_country step

--- tests/server/main/init_country_test.py
+++ tests/server/main/init_country_test.py
@@ -1,7 +1,7 @@
 from elasticsearch import Elasticsearch
 from matcher.server.main.config import config
 from matcher.server.main.init_country import get_cities_from_country, get_info_from_country, \
-    get_stop_words_from_country, init_country
+    get_stop_words_from_country, get_universities_from_country, init_country
 
 
 class TestInitCountry:
@@ -9,17 +9,34 @@ class TestInitCountry:
         result = get_info_from_country('fr')
         assert result == {'alpha_2': 'FR', 'alpha_3': 'FRA', 'info': ['France', 'French']}
 
-    def test_get_cities_from_country(self):
+    def test_get_cities_from_country_fr(self):
         french_cities = get_cities_from_country('fr')['cities']
         assert len(french_cities) == 75
         assert 'paris' in french_cities
         assert 'nancy' in french_cities
         assert 'issy-les-moulineaux' in french_cities
 
-    def test_get_stop_words_from_country(self):
+    def test_get_cities_from_country_cn(self):
+        chinese_cities = get_cities_from_country('cn')['cities']
+        assert len(chinese_cities) == 0
+
+    def test_get_universities_from_country_fr(self):
+        french_universities = get_universities_from_country('fr')['universities']
+        assert len(french_universities) == 333
+        assert 'Rennes School of Business' in french_universities
+
+    def test_get_universities_from_country_cn(self):
+        chinese_universities = get_universities_from_country('cn')['universities']
+        assert len(chinese_universities) == 0
+
+    def test_get_stop_words_from_country_fr(self):
         french_stop_words = get_stop_words_from_country('fr')['stop_words']
         assert len(french_stop_words) == 14
         assert 'paris.*tx' in french_stop_words
+
+    def test_get_stop_words_from_country_cn(self):
+        chinese_stop_words = get_stop_words_from_country('cn')['stop_words']
+        assert len(chinese_stop_words) == 0
 
     def test_init_country(self):
         es = Elasticsearch(config['ELASTICSEARCH_HOST'])
@@ -34,3 +51,4 @@ class TestInitCountry:
         assert french_result['alpha_3'] == 'FRA'
         assert len(french_result['info']) == 2
         assert len(french_result['stop_words']) == 14
+        assert len(french_result['universities']) == 333
