5cb214b314e526afde6add738bf67a7fcf1f11b5
Anne L'Hôte <anne.lhote@gmail.com>
2021-04-28 10:05:49 +0200

    [#7] Add universities on match_country step

--- matcher/server/main/match_country.py
+++ matcher/server/main/match_country.py
@@ -22,10 +22,11 @@ def get_regex_from_country_by_fields(es: Elasticsearch = None, index: str = '', 
             values = []
         regexes = regexes + values
     if is_complex:
-        pattern = '|'.join(['(?<![a-z])' + regex + '(?![a-z])' for regex in regexes])
+        pattern = '|'.join(['(?<![a-z])' + normalize_text(regex, remove_sep=False) + '(?![a-z])' for regex in regexes])
     else:
-        pattern = '|'.join([regex for regex in regexes])
-    return re.compile(pattern, re.IGNORECASE) if pattern != '' else None
+        pattern = '|'.join([normalize_text(regex, remove_sep=False) for regex in regexes])
+    print(pattern)
+    return re.compile(pattern, re.IGNORECASE | re.UNICODE) if pattern != '' else None
 
 
 def get_countries_from_query(query: str = '') -> list:
@@ -34,7 +35,8 @@ def get_countries_from_query(query: str = '') -> list:
     query = normalize_text(query, remove_sep=False)
     for country in pycountry.countries:
         country = country.alpha_2.lower()
-        keywords_regex = get_regex_from_country_by_fields(es, ES_INDEX, country, ['cities', 'info'], True)
+        keywords_regex = get_regex_from_country_by_fields(es, ES_INDEX, country, ['cities', 'info', 'universities'],
+                                                          True)
         if keywords_regex is not None and re.search(keywords_regex, query):
             stop_words_regex = get_regex_from_country_by_fields(es, ES_INDEX, country, ['stop_words'], False)
             if stop_words_regex is not None and re.search(stop_words_regex, query):
