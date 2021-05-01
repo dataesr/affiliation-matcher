2e1567e66f3f2b6a862f88a7063fe9e347ba391d
Anne L'Hôte <anne.lhote@gmail.com>
2021-05-01 13:14:12 +0200

    [country] Collect universities from wikidata

--- matcher/server/main/init_country.py
+++ matcher/server/main/init_country.py
@@ -9,8 +9,8 @@ from matcher.server.main.config import config
 ES_INDEX = 'country'
 FILE_COUNTRY_FORBIDDEN = 'country_forbidden.json'
 FILE_COUNTRY_WHITE_LIST = 'country_white_list.json'
-FILE_FRENCH_UNIVERSITIES = 'fr_universities.json'
 QUERY_CITY_POPULATION_LIMIT = 50000
+WIKIDATA_SPARQL_URL = 'https://query.wikidata.org/bigdata/namespace/wdq/sparql'
 
 
 def get_all_cities() -> dict:
@@ -29,8 +29,7 @@ def get_all_cities() -> dict:
         FILTER(?population > ''' + str(QUERY_CITY_POPULATION_LIMIT) + ''') .
     }
     '''
-    url = 'https://query.wikidata.org/bigdata/namespace/wdq/sparql'
-    response = requests.get(url, params={'query': query, 'format': 'json'})
+    response = requests.get(WIKIDATA_SPARQL_URL, params={'query': query, 'format': 'json'})
     results = {}
     if response.status_code == requests.codes.ok:
         for city in response.json()['results']['bindings']:
@@ -82,6 +81,62 @@ def get_all_cities() -> dict:
     return results
 
 
+def get_all_universities() -> dict:
+    query = '''
+    SELECT DISTINCT ?country_alpha2 ?label_native ?label_en ?label_fr ?label_es ?label_it WHERE {
+        ?university wdt:P31/wdt:P279* wd:Q38723 ;
+                  wdt:P17 ?country .
+        ?country wdt:P297 ?country_alpha2 .
+        OPTIONAL { ?university wdt:P1705 ?label_native . }
+        OPTIONAL { ?university rdfs:label ?label_en filter (lang(?label_en) = "en") . }
+        OPTIONAL { ?university rdfs:label ?label_fr filter (lang(?label_fr) = "fr") . }
+        OPTIONAL { ?university rdfs:label ?label_es filter (lang(?label_es) = "es") . }
+        OPTIONAL { ?university rdfs:label ?label_it filter (lang(?label_it) = "it") . }
+    }
+    '''
+    response = requests.get(WIKIDATA_SPARQL_URL, params={'query': query, 'format': 'json'})
+    results = {}
+    if response.status_code == requests.codes.ok:
+        for university in response.json()['results']['bindings']:
+            alpha_2 = university['country_alpha2']['value'].lower()
+            if alpha_2 not in results.keys():
+                results[alpha_2] = {}
+                results[alpha_2]['all'] = []
+                results[alpha_2]['en'] = []
+                results[alpha_2]['fr'] = []
+                results[alpha_2]['es'] = []
+                results[alpha_2]['it'] = []
+            if 'label_native' in university.keys():
+                results[alpha_2]['all'].append(university['label_native']['value'])
+            if 'label_en' in university.keys():
+                results[alpha_2]['all'].append(university['label_en']['value'])
+                results[alpha_2]['en'].append(university['label_en']['value'])
+            if 'label_fr' in university.keys():
+                results[alpha_2]['all'].append(university['label_fr']['value'])
+                results[alpha_2]['fr'].append(university['label_fr']['value'])
+            if 'label_es' in university.keys():
+                results[alpha_2]['all'].append(university['label_es']['value'])
+                results[alpha_2]['es'].append(university['label_es']['value'])
+            if 'label_it' in university.keys():
+                results[alpha_2]['all'].append(university['label_it']['value'])
+                results[alpha_2]['it'].append(university['label_it']['value'])
+    else:
+        # TODO: use logger
+        print('The request returned an error')
+        print(response.status_code)
+    for country in results:
+        results[country]['all'] = list(set(results[country]['all']))
+        if 'en' in results[country].keys():
+            results[country]['en'] = list(set(results[country]['en']))
+        if 'fr' in results[country].keys():
+            results[country]['fr'] = list(set(results[country]['fr']))
+        if 'es' in results[country].keys():
+            results[country]['es'] = list(set(results[country]['es']))
+        if 'it' in results[country].keys():
+            results[country]['it'] = list(set(results[country]['it']))
+    return results
+
+
 def get_info_from_country(alpha_2: str = None) -> dict:
     country = pycountry.countries.get(alpha_2=alpha_2)
     info = []
@@ -93,22 +148,6 @@ def get_info_from_country(alpha_2: str = None) -> dict:
         info.append(country.common_name)
     return {'alpha_2': country.alpha_2, 'alpha_3': country.alpha_3,
             'info': info}
-
-
-def get_universities_from_country(alpha_2: str = None) -> dict:
-    universities = []
-    if alpha_2 == 'fr':
-        dirname = os.path.dirname(__file__)
-        with open(os.path.join(dirname, FILE_FRENCH_UNIVERSITIES), 'r') as file:
-            data = json.load(file)
-            for d in data:
-                uo_lib_officiel = d['fields']['uo_lib_officiel']
-                if uo_lib_officiel not in universities:
-                    universities.append(uo_lib_officiel)
-                uo_lib = d['fields']['uo_lib']
-                if uo_lib not in universities:
-                    universities.append(uo_lib)
-    return {'universities': universities}
 
 
 def get_white_list_from_country(alpha_2: str = None) -> dict:
@@ -141,6 +180,7 @@ def init_country() -> None:
     es.indices.create(index=ES_INDEX, body=mapping, ignore=400)
     es.delete_by_query(index=ES_INDEX, body={'query': {'match_all': {}}}, refresh=True)
     all_cities = get_all_cities()
+    all_universities = get_all_universities()
     # TODO: use helpers.parallel_bulk
     for country in pycountry.countries:
         country = country.alpha_2.lower()
@@ -148,9 +188,9 @@ def init_country() -> None:
         info = get_info_from_country(country)
         body.update(info)
         cities_all = all_cities[country]['all'] if country in all_cities.keys() and 'all' in \
-                                                                                    all_cities[country].keys() else []
+            all_cities[country].keys() else []
         cities_strict = all_cities[country]['strict'] if country in all_cities.keys() and 'strict' in \
-                                                                                    all_cities[country].keys() else []
+            all_cities[country].keys() else []
         cities_en = all_cities[country]['en'] if country in all_cities.keys() and 'en' in all_cities[country].keys() \
             else []
         cities_fr = all_cities[country]['fr'] if country in all_cities.keys() and 'fr' in all_cities[country].keys() \
@@ -161,8 +201,18 @@ def init_country() -> None:
             else []
         body.update({'cities': cities_all, 'cities_strict': cities_strict,'cities_en': cities_en,
                      'cities_fr': cities_fr, 'cities_es': cities_es, 'cities_it': cities_it})
-        universities = get_universities_from_country(country)
-        body.update(universities)
+        universities_all = all_universities[country]['all'] if country in all_universities.keys() and 'all' in \
+            all_universities[country].keys() else []
+        universities_en = all_universities[country]['en'] if country in all_universities.keys() and 'en' in \
+            all_universities[country].keys() else []
+        universities_fr = all_universities[country]['fr'] if country in all_universities.keys() and 'fr' in \
+            all_universities[country].keys() else []
+        universities_es = all_universities[country]['es'] if country in all_universities.keys() and 'es' in \
+            all_universities[country].keys() else []
+        universities_it = all_universities[country]['it'] if country in all_universities.keys() and 'it' in \
+            all_universities[country].keys() else []
+        body.update({'universities': universities_all, 'universities_en': universities_en, 'universities_fr':
+                    universities_fr, 'universities_es': universities_es, 'universities_it': universities_it})
         white_list = get_white_list_from_country(country)
         body.update(white_list)
         stop_words = get_stop_words_from_country(country)
