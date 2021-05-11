0a9e7967666035e6d1cb2ba1dc641da701ed66aa
Anne L'Hôte <anne.lhote@gmail.com>
2021-05-11 08:41:04 +0200

    [clean][refactor] match_rnsr

--- matcher/server/main/match_rnsr.py
+++ matcher/server/main/match_rnsr.py
@@ -1,18 +1,17 @@
 import requests
 
 from bs4 import BeautifulSoup
-from elasticsearch import Elasticsearch
 from elasticsearch_dsl import Search
 
 from matcher.server.main.config import config
+from matcher.server.main.my_elastic import MyElastic
 
-es = Elasticsearch(config['ELASTICSEARCH_HOST'])
+es = MyElastic()
 
 
 def normalize_for_count(x, matching_field):
     if 'code' in matching_field:
         return x.upper()
-
     analyzer = None
     if matching_field in ['name', 'acronym']:
         analyzer = "analyzer_{}".format(matching_field)
@@ -22,7 +21,6 @@ def normalize_for_count(x, matching_field):
         analyzer = "analyzer_supervisor_acronym"
     elif matching_field in ['supervisors_name']:
         analyzer = "analyzer_supervisor"
-
     if analyzer:
         try:
             r = requests.post(config['ELASTICSEARCH_URL'] + 'index-rnsr-all/_analyze', json={
@@ -35,7 +33,7 @@ def normalize_for_count(x, matching_field):
     return x.lower()[0:6]
 
 
-def match_unstructured(year, query='') -> dict:
+def match_unstructured(year: str = '2020', query: str = '') -> dict:
     matching_info = {
         'code': get_match_code(year, query),
         'code_digit': get_match_code_digit(year, query),
@@ -47,7 +45,6 @@ def match_unstructured(year, query='') -> dict:
         'name': get_match_name(year, query),
         'acronym': get_match_acronym(year, query)
     }
-
     strategies = [
         'code;supervisors_acronym;supervisors_name;city',
         'code;supervisors_name;city',
@@ -74,11 +71,8 @@ def match_unstructured(year, query='') -> dict:
         'acronym;city',
         'code_fuzzy;city'
     ]
-
-    logs = ''
-    logs += '<h1> &#128269; {}</h1>'.format(query)
-
-    return match_structured(matching_info, strategies, logs)
+    logs = '<h1> &#128269; {query}</h1>'.format(query=query)
+    return match_structured(matching_info=matching_info, strategies=strategies, logs=logs)
 
 
 # Todo: Deprecated
@@ -90,7 +84,6 @@ def match_fields(year, code, name, city, acronym, supervisors_id) -> dict:
         'acronym': get_match_acronym(year, acronym),
         'supervisors_id': get_match_supervisors_id(year, supervisors_id)
     }
-
     strategies = [
         'code;city;name;acronym;supervisors_id',
         'code;city;name;supervisors_id',
@@ -100,21 +93,16 @@ def match_fields(year, code, name, city, acronym, supervisors_id) -> dict:
         'city;name;acronym;supervisors_id',
         'city;acronym;supervisors_id'
     ]
-
-    logs = ''
-
-    return match_structured(matching_info, strategies, logs)
+    return match_structured(matching_info=matching_info, strategies=strategies)
 
 
-def match_structured(matching_info, strategies, logs) -> dict:
+def match_structured(matching_info: dict = None, strategies: list = None, logs: str = '') -> dict:
     has_code = False
     if len(matching_info.get('code', {}).get('ids', [])) > 0:
         has_code = True
-
     has_acronym = False
     if len(matching_info.get('acronym', {}).get('ids', [])) > 0:
         has_acronym = True
-
     all_matches = {}
     field_matches = {}
     min_match_for_field = {}
@@ -126,30 +114,23 @@ def match_structured(matching_info, strategies, logs) -> dict:
             all_matches[match_id] += matching_info[f].get('nb_matches', {})[match_id]
             if f not in field_matches[match_id]:
                 field_matches[match_id].append(f)
-
         min_match_for_field[f] = 1
-
     min_match_for_field['supervisors_name'] = 3
     min_match_for_field['supervisors_acronym'] = 2
-
     final_results = {}
     forbidden_id = []
-
     logs += "<ol> "
     for strat in strategies:
         current_strat_answers = []
         current_strat_avoid = []
         strat_fields = strat.split(';')
         logs += "<li>Strategie testée : {}".format(strat)
-
         indiv_ids = [matching_info[field]['ids'] for field in strat_fields]
         strat_ids = set(indiv_ids[0]).intersection(*indiv_ids)
-
         if len(strat_ids) == 0:
             logs += " &empty; </li>"
             continue
         logs += "</li></br>"
-
         max_number = {}
         logs += "<ol> "
         for potential_id in strat_ids:
@@ -157,35 +138,24 @@ def match_structured(matching_info, strategies, logs) -> dict:
             current_match = {'id': potential_id}
             for field in strat_fields:
                 current_match[field + '_match'] = 0
-                # probleme avec les highlights
                 if potential_id in matching_info[field]['nb_matches']:
                     current_match[field + '_match'] = matching_info[field]['nb_matches'][potential_id]
-
                     current_highlights = matching_info[field]['highlights'][potential_id]
-                    current_highlights = [e.replace('<em>', '<strong>').replace('</em>', '</strong>') for e in
-                                          current_highlights]
+                    current_highlights = [highlight.replace('<em>', '<strong>').replace('</em>', '</strong>')
+                                          for highlight in current_highlights]
                     logs += "     - {} {} : {}<br/>".format(
                         matching_info[field]['nb_matches'][potential_id],
                         field,
                         current_highlights)
-
                 if field not in max_number:
                     max_number[field] = 0
-                    # if field == 'name':
-                    #    max_number[field] = 2
-
                 max_number[field] = max(max_number[field], current_match[field + '_match'])
-
             current_strat_answers.append(current_match)
-
         if len(max_number) > 0:
             logs += "<li> &#9989; Nombre de match par champ : {}<br/></li>".format(max_number)
-
-        logs += "</ol>"  # end of potential ids
-
+        logs += "</ol>"
         if len(strat_ids) == 0:
             continue
-
         current_potential_ids = strat_ids
         retained_id_for_strat = []
         logs += "Parcours des champs de la stratégie :"
@@ -201,8 +171,8 @@ def match_structured(matching_info, strategies, logs) -> dict:
                         if max_number[field] >= min_match_for_field[field]:
                             retained_id_for_strat.append(potential_id)
                         else:
-                            logs += "<br/> &#128584; " + potential_id + " ignoré car {} {} est insuffisant ({} attendus au min)".format(
-                                max_number[field], field, min_match_for_field[field])
+                            logs += "<br/> &#128584; " + potential_id + " ignoré car {} {} est insuffisant ({} " \
+                                    "attendus au min)".format(max_number[field], field, min_match_for_field[field])
                             current_strat_avoid.append(potential_id)
                     elif potential_id not in matching_info.get('code', {}).get('ids', []):
                         logs += "<br/> &#10060; {} ajouté à la black-list car seulement {} {} vs le max est {}".format(
@@ -218,9 +188,6 @@ def match_structured(matching_info, strategies, logs) -> dict:
                     break
                 else:
                     pass
-                    # if verbose:
-                    # print("not stopping because strategy has no code or acronym")
-                    # print(matching_info.get('name',{}).get('highlights', {}).get(potential_id))
             else:
                 current_potential_ids = retained_id_for_strat
                 retained_id_for_strat = []
@@ -228,14 +195,11 @@ def match_structured(matching_info, strategies, logs) -> dict:
         for x in current_strat_avoid:
             if x in final_results[strat]:
                 final_results[strat].remove(x)
-
-        # for res in final_results:
         if len(final_results[strat]) == 1:
             logs += "<br/> 1&#65039;&#8419; unique match pour cette stratégie : {} ".format(final_results[strat][0])
             if final_results[strat][0] in forbidden_id:
                 logs += "&#10060; car dans la black-list"
                 continue
-
             if has_code:
                 if final_results[strat][0] in matching_info.get('code', {}).get('ids', []):
                     logs += " &#128076; car a bien un label numéro <br/>"
@@ -265,7 +229,6 @@ def match_structured(matching_info, strategies, logs) -> dict:
                 else:
                     logs += " &#128078; car n'a pas l'acronyme"
                     continue
-
             else:
                 logs += " &#128076;<br/>"
                 logs += "<h3>{}</h3>".format(final_results[strat][0])
@@ -273,124 +236,107 @@ def match_structured(matching_info, strategies, logs) -> dict:
                 return_val = {'match': return_id, 'logs': logs}
                 return_val.update(get_supervisors(return_id))
                 return return_val
-
     return {'match': None, 'logs': logs}
 
 
-def get_match_code(year, query, verbose=False) -> dict:
-    return get_info(year, query, ['code_numbers'], size=20, verbose=verbose, highlights=['code_numbers'])
+def get_match_code(year, query) -> dict:
+    return get_info(year, query, ['code_numbers'], size=20, highlights=['code_numbers'])
 
 
-def get_match_code_label(year, query, verbose=False) -> dict:
-    return get_info(year, query, ['code_numbers.labels'], size=10000, verbose=verbose,
-                    highlights=['code_numbers.labels'])
+def get_match_code_label(year, query) -> dict:
+    return get_info(year, query, ['code_numbers.labels'], size=10000, highlights=['code_numbers.labels'])
 
 
-def get_match_code_digit(year, query, verbose=False) -> dict:
-    return get_info(year, query, ['code_numbers.digits'], size=20, verbose=verbose, highlights=['code_numbers.digits'])
+def get_match_code_digit(year, query) -> dict:
+    return get_info(year, query, ['code_numbers.digits'], size=20, highlights=['code_numbers.digits'])
 
 
-def get_match_code_fuzzy(year, query, verbose=False) -> dict:
-    return get_info(year, query, ['code_numbers', 'code_numbers.digits'], size=1000, verbose=verbose,
+def get_match_code_fuzzy(year, query) -> dict:
+    return get_info(year, query, ['code_numbers', 'code_numbers.digits'], size=1000,
                     highlights=['code_numbers', 'code_numbers.digits'], fuzzy_ok=True)
 
 
-def get_match_city(year, query, verbose=False) -> dict:
-    return get_info(year, query, ['addresses'], size=5000, verbose=verbose, highlights=['addresses'])
+def get_match_city(year, query) -> dict:
+    return get_info(year, query, ['addresses'], size=5000, highlights=['addresses'])
 
 
-def get_match_name(year, query, verbose=False) -> dict:
-    return get_info(year, query, ['names'], size=200, verbose=verbose, highlights=['names'])
+def get_match_name(year, query) -> dict:
+    return get_info(year, query, ['names'], size=200, highlights=['names'])
 
 
-def get_match_acronym(year, query, verbose=False) -> dict:
-    return get_info(year, query, ['acronyms'], size=5000, verbose=verbose, highlights=['acronyms'])
+def get_match_acronym(year, query) -> dict:
+    return get_info(year, query, ['acronyms'], size=5000, highlights=['acronyms'])
 
 
-def get_match_supervisors_name(year, query, verbose=False) -> dict:
-    return get_info(year, query, ['supervisors_name'], size=10000, verbose=verbose, highlights=['supervisors_name'])
+def get_match_supervisors_name(year, query) -> dict:
+    return get_info(year, query, ['supervisors_name'], size=10000, highlights=['supervisors_name'])
 
 
-def get_match_supervisors_id(year, query, verbose=False) -> dict:
-    return get_info(year, query, ['supervisors_id'], size=2000, verbose=verbose, highlights=['supervisors_id'])
+def get_match_supervisors_id(year, query) -> dict:
+    return get_info(year, query, ['supervisors_id'], size=2000, highlights=['supervisors_id'])
 
 
-def get_match_supervisors_acronym(year, query, verbose=False) -> dict:
-    return get_info(year, query, ['supervisors_acronym'], size=2000, verbose=verbose,
-                    highlights=['supervisors_acronym'])
+def get_match_supervisors_acronym(year, query) -> dict:
+    return get_info(year, query, ['supervisors_acronym'], size=2000, highlights=['supervisors_acronym'])
 
 
-def get_info(year, query, search_fields, size=20, verbose=False, highlights=None, fuzzy_ok=False) -> dict:
+def get_info(year, query: str = None, search_fields: list = None, size=20, highlights: list = None,
+             fuzzy_ok=False) -> dict:
+    if query is None:
+        query = ''
+    if search_fields is None:
+        search_fields = []
     if highlights is None:
         highlights = []
-    if query is None:
-        query = ""
-    my_index = "index-rnsr-{}".format(year)
-    s = Search(using=es, index=my_index)
+    index = 'index-rnsr-{year}'.format(year=year)
+    s = Search(using=es, index=index)
     for f in highlights:
         s = s.highlight(f)
-
     if search_fields == ['names']:
-        s = s.query("multi_match", query=query,
-                    minimum_should_match=1,
-                    fuzziness="auto",
-                    fields=search_fields)
+        s = s.query('multi_match', query=query, minimum_should_match=1, fuzziness='auto', fields=search_fields)
     else:
-        s = s.query("multi_match", query=query,
-                    minimum_should_match=1,
-                    fields=search_fields)
+        s = s.query('multi_match', query=query, minimum_should_match=1, fields=search_fields)
     s = s[0:size]
     results = s.execute()
     hits = results.hits
-
     if len(hits) == 0 and fuzzy_ok:
-        if verbose:
-            print("fuzzy {}".format(search_fields))
-        s = Search(using=es, index=my_index)
+        s = Search(using=es, index=index)
         for f in highlights:
             s = s.highlight(f)
-        s = s.query("multi_match", query=query,
+        s = s.query('multi_match', query=query,
                     fuzziness=1,
                     fields=search_fields)
         s = s[0:size]
         results = s.execute()
         hits = results.hits
-
-    res_ids = []
+    ids = []
     scores = []
     highlights = {}
     nb_matches = {}
     matches_frag = {}
-
     for hit in hits:
-
         if search_fields == ['names'] and hit.meta.score < hits.max_score * 0.5:
             continue
-
-        res_ids.append(hit.id)
+        ids.append(hit.id)
         scores.append(hit.meta.score)
         highlights[hit.id] = []
-
         for matching_field in hit.meta.highlight:
             for fragment in hit.meta.highlight[matching_field]:
                 highlights[hit.id].append(fragment)
-
                 matches = [normalize_for_count(e.get_text(), matching_field) for e in
-                           BeautifulSoup(fragment, "lxml").find_all("em")]
-
+                           BeautifulSoup(fragment, 'lxml').find_all('em')]
                 if hit.id not in nb_matches:
                     nb_matches[hit.id] = 0
                     matches_frag[hit.id] = []
                 matches_frag[hit.id] += matches
                 matches_frag[hit.id] = list(set(matches_frag[hit.id]))
                 nb_matches[hit.id] = len(matches_frag[hit.id])
-
-    return {"ids": res_ids, "highlights": highlights, "nb_matches": nb_matches}
+    return {'ids': ids, 'highlights': highlights, 'nb_matches': nb_matches}
 
 
 def get_supervisors(rnsr_id) -> dict:
-    my_index = config['ELASTICSEARCH_INDEX']
-    s = Search(using=es, index=my_index)
+    index = config['ELASTICSEARCH_INDEX']
+    s = Search(using=es, index=index)
     s = s.query('multi_match', query=rnsr_id, fields='id')
     hit = s.execute().hits[0]
     return {
