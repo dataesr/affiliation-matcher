a60ce04ff0f31a1cc8bd8c3b6e4d62fc02a39670
Anne L'Hôte <anne.lhote@gmail.com>
2021-06-01 15:43:30 +0200

    [ci] Fix tests

--- matcher/server/main/init_country.py
+++ matcher/server/main/init_country.py
@@ -84,7 +84,7 @@ def get_cities_from_wikidata() -> dict:
                 results[alpha_2]['all'].append(city['label_it']['value'])
                 results[alpha_2]['it'].append(city['label_it']['value'])
     else:
-        logger.error('The request returned an error. Code : {code}'.format(code=response.status_code))
+        logger.error(f'The request returned an error. Code : {response.status_code}')
     for country in results:
         results[country]['all'] = list(set(results[country]['all']))
         if 'strict' in results[country].keys():
