2c7746edddce3aac1dc7bf28986861a9661aac11
Anne L'Hôte <anne.lhote@gmail.com>
2021-06-16 14:14:11 +0200

    [feat] Query ES for the country matcher
    Instead of building regex

--- matcher/server/main/match_country.py
+++ matcher/server/main/match_country.py
@@ -1,54 +1,27 @@
-import re
-
-import pycountry
-
 from matcher.server.main.my_elastic import MyElastic
 from matcher.server.main.utils import normalize_text
 
 ES_INDEX = 'country'
 
 
-def get_regex_from_country_by_fields(es: MyElastic = None, index: str = '', country: str = '', fields: list = None,
-                                     is_complex: bool = False):
-    country = country.lower()
-    results = es.search(index=index, body={'query': {'match': {'alpha_2': country}}})
-    regexes = []
-    for field in fields:
-        try:
-            values = results['hits']['hits'][0]['_source'][field]
-        except (KeyError, IndexError):
-            values = []
-        values = values if type(values) == list else [values]
-        regexes = regexes + values
-    if is_complex:
-        pattern = '|'.join(
-            ['(?<![a-z])' + normalize_text(regex, remove_separator=False) + '(?![a-z])' for regex in regexes])
-    else:
-        pattern = '|'.join([normalize_text(regex, remove_separator=False) for regex in regexes])
-    return re.compile(pattern, re.IGNORECASE | re.UNICODE) if pattern != '' else None
-
-
 def get_countries_from_query(query: str = '', strategies: list = None, index: str = ES_INDEX) -> list:
     if strategies is None:
         strategies = [['names']]
     es = MyElastic()
-    query = normalize_text(query, remove_separator=False)
-    for criteria in strategies:
-        countries = []
-        for country in pycountry.countries:
-            is_country_matched = True
-            country = country.alpha_2.lower()
-            for criterion in criteria:
-                keywords_regex = get_regex_from_country_by_fields(es=es, index=index, country=country,
-                                                              fields=[criterion], is_complex=True)
-                is_country_matched = is_country_matched and keywords_regex and bool(re.search(keywords_regex, query))
-            stop_words_regex = get_regex_from_country_by_fields(es=es, index=index, country=country,
-                                                            fields=['stop_words'], is_complex=False)
-            if stop_words_regex and re.search(stop_words_regex, query):
-                continue
-            if is_country_matched:
-                countries.append(country)
-        countries = list(set(countries))
-        if len(countries) > 0:
-            return countries
+    normalized_query = normalize_text(query, remove_separator=False)
+    for strategy in strategies:
+        strategy_results = None
+        for criteria in strategy:
+            body = {'query': {'match': {criteria: normalized_query}}, '_source': {'includes': ['alpha_2']},
+                    'highlight': {'fields': {criteria: {}}}}
+            hits = es.search(index=index, body=body).get('hits', []).get('hits', [])
+            criteria_results = [hit.get('_source', {}).get('alpha_2').lower() for hit in hits]
+            if strategy_results is None:
+                strategy_results = criteria_results
+            else:
+                # Union
+                strategy_results = list(set(strategy_results) & set(criteria_results))
+        # Strategies stopped as soon as a first result is met
+        if len(strategy_results) > 0:
+            return strategy_results
     return []
