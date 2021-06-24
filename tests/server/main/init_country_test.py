31e3d1a9d844243efd6b5251c1019d3a649ce4f1
Anne L'Hôte <anne.lhote@gmail.com>
2021-06-24 18:34:45 +0200

    [feat] Create the init point for wikidata data

--- tests/server/main/init_country_test.py
+++ tests/server/main/init_country_test.py
@@ -1,8 +1,7 @@
 import pytest
 
-from matcher.server.main.init_country import get_cities_from_insee, get_cities_from_wikidata,\
-    get_universities_from_mesri, get_universities_from_wikidata, get_names_from_country, get_hospitals_from_wikidata,\
-    init_country
+from matcher.server.main.init_country import get_cities_from_insee, get_universities_from_mesri,\
+    get_names_from_country, init_country
 from matcher.server.main.my_elastic import MyElastic
 
 
@@ -27,41 +26,6 @@ class TestInitCountry:
                                                                            'Plurinational State of Bolivia', 'Bolivia'],
                           'name': 'Bolivia'}
 
-    def test_get_cities_from_wikidata(self) -> None:
-        cities = get_cities_from_wikidata()
-        assert len(cities) == 178
-        assert set(cities['fr'].keys()) == {'all', 'strict', 'en', 'fr', 'es', 'it'}
-        assert 'Dunkirk' in cities['fr']['all']
-        assert 'Dunkerque' in cities['fr']['all']
-        assert 'Dunkirk' in cities['fr']['en']
-        assert 'Dunkirk' not in cities['fr']['fr']
-        assert 'Dunkerque' in cities['fr']['fr']
-        assert 'Dunkerque' not in cities['fr']['en']
-
-    def test_get_universities_from_wikidata(self) -> None:
-        universities = get_universities_from_wikidata()
-        assert len(universities) == 216
-        assert set(universities['fr'].keys()) == {'all', 'en', 'fr', 'es', 'it'}
-        assert 'École normale supérieure de Fontenay-Saint-Cloud' in universities['fr']['all']
-        assert 'École normale supérieure de Fontenay-Saint-Cloud' in universities['fr']['en']
-        assert 'École normale supérieure de Fontenay-Saint-Cloud' in universities['fr']['fr']
-        assert 'Lille Catholic University' in universities['fr']['all']
-        assert 'Lille Catholic University' in universities['fr']['en']
-        assert 'Lille Catholic University' not in universities['fr']['fr']
-        assert 'université catholique de Lille' in universities['fr']['all']
-        assert 'université catholique de Lille' in universities['fr']['fr']
-        assert 'université catholique de Lille' not in universities['fr']['en']
-
-    def test_get_hospitals_from_wikidata(self) -> None:
-        hospitals = get_hospitals_from_wikidata()
-        assert len(hospitals) == 204
-        assert set(hospitals['fr'].keys()) == {'all', 'en', 'fr', 'es', 'it'}
-        assert 'Paul Morel Hospital' in hospitals['fr']['all']
-        assert 'Paul Morel Hospital' in hospitals['fr']['en']
-        assert 'Paul Morel Hospital' not in hospitals['fr']['fr']
-        assert 'hôpital Paul-Morel' in hospitals['fr']['fr']
-        assert 'hôpital Paul-Morel' not in hospitals['fr']['en']
-
     @pytest.fixture(scope='class')
     def setup(self) -> None:
         index = 'country-test'
@@ -69,14 +33,7 @@ class TestInitCountry:
         yield {'es': es, 'index': index}
         es.delete_index(index=index)
 
-    def test_init_country(self, setup, requests_mock) -> None:
-        requests_mock.real_http = True
-        requests_mock.get('https://query.wikidata.org/bigdata/namespace/wdq/sparql',
-                          json={'results': {'bindings': [
-                              {'country_alpha2': {'value': 'fr'}, 'label_native': {'value': 'value_01'}},
-                              {'country_alpha2': {'value': 'de'}, 'label_native': {'value': 'value_02'}},
-                              {'country_alpha2': {'value': 'fr'}, 'label_native': {'value': 'value_03'}}
-                          ]}})
+    def test_init_country(self, setup) -> None:
         index = setup['index']
         es = setup['es']
         init_country(index=index)
@@ -88,7 +45,4 @@ class TestInitCountry:
         assert french_result['alpha_2'] == 'FR'
         assert french_result['alpha_3'] == 'FRA'
         assert len(french_result['all_names']) == 2
-        assert len(french_result['wikidata_cities']) == 2
-        assert len(french_result['wikidata_hospitals']) == 2
-        assert len(french_result['wikidata_universities']) == 2
         es.delete_index(index=index)
