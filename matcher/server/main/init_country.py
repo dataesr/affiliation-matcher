35e25864e1c4dce753b24a476784f8d5d82331e5
Anne L'Hôte <anne.lhote@gmail.com>
2021-06-24 18:38:51 +0200

    [feat] Remove data from insee and mesri from the country index

--- matcher/server/main/init_country.py
+++ matcher/server/main/init_country.py
@@ -1,31 +1,11 @@
-import json
-import os
-
 import pycountry
 
 from matcher.server.main.logger import get_logger
 from matcher.server.main.my_elastic import MyElastic
 
 ES_INDEX = 'country'
-FILE_FR_CITIES_INSEE = 'fr_cities_insee.csv'
-FILE_FR_UNIVERSITIES_MESRI = 'fr_universities_mesri.json'
 
 logger = get_logger(__name__)
-
-
-def get_cities_from_insee() -> dict:
-    dirname = os.path.dirname(__file__)
-    with open(os.path.join(dirname, FILE_FR_CITIES_INSEE), 'r') as file:
-        insee_cities = [line.rstrip() for line in file]
-    return {'insee_cities': insee_cities}
-
-
-def get_universities_from_mesri() -> dict:
-    dirname = os.path.dirname(__file__)
-    with open(os.path.join(dirname, FILE_FR_UNIVERSITIES_MESRI), 'r') as file:
-        data = json.load(file)
-        mesri_universities = [university['fields']['uo_lib_officiel'] for university in data]
-    return {'mesri_universities': mesri_universities}
 
 
 def get_names_from_country(alpha_2: str = None) -> dict:
@@ -74,13 +54,7 @@ def init_country(index: str = ES_INDEX) -> None:
     for country in pycountry.countries:
         country = country.alpha_2.lower()
         body = {'_index': index}
-        # GENERAL NAMES
         names = get_names_from_country(alpha_2=country)
         body.update(names)
-        if country == 'fr':
-            # INSEE CITIES
-            body.update(get_cities_from_insee())
-            # MESRI UNIVERSITIES
-            body.update(get_universities_from_mesri())
         actions.append(body)
     es.parallel_bulk(actions=actions)
--- tests/server/main/init_country_test.py
+++ tests/server/main/init_country_test.py
@@ -1,20 +1,10 @@
 import pytest
 
-from matcher.server.main.init_country import get_cities_from_insee, get_universities_from_mesri,\
-    get_names_from_country, init_country
+from matcher.server.main.init_country import get_names_from_country, init_country
 from matcher.server.main.my_elastic import MyElastic
 
 
 class TestInitCountry:
-    def test_get_cities_from_insee(self) -> None:
-        result = get_cities_from_insee()['insee_cities']
-        assert len(result) == 75
-        assert 'La Rochelle' in result
-
-    def test_get_universities_from_mesri(self) -> None:
-        result = get_universities_from_mesri()['mesri_universities']
-        assert len(result) == 265
-
     def test_get_names_from_country_fr(self) -> None:
         result = get_names_from_country('fr')
         assert result == {'alpha_2': 'FR', 'alpha_3': 'FRA', 'all_names': ['France', 'French Republic'],
