961b2f94f8cfdeb67f0c5f204f24b62136fcec41
Anne L'Hôte <anne.lhote@gmail.com>
2021-06-25 16:31:25 +0200

    [fix] Filter empty prefix on get_index_name

--- matcher/server/main/load_rnsr.py
+++ matcher/server/main/load_rnsr.py
@@ -13,7 +13,8 @@ SOURCE = 'rnsr'
 
 
 def get_index_name(index_name: str, index_prefix: str = '') -> str:
-    return '_'.join([index_prefix, SOURCE, index_name])
+    names = list(filter(lambda x: x != '', [index_prefix, SOURCE, index_name]))
+    return '_'.join(names)
 
 
 def get_mappings(analyzer) -> dict:
