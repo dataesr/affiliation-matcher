2b1b9f1d092b3da635cffc1e8455fe672b205b8e
Anne L'Hôte <anne.lhote@gmail.com>
2021-06-25 11:15:14 +0200

    [refactor] Set ES index name as singular

--- tests/server/main/init_grid_test.py
+++ tests/server/main/init_grid_test.py
@@ -6,11 +6,11 @@ class TestInitGrid:
     def test_init_country(self) -> None:
         es = MyElastic()
         init_grid(index_prefix='test_')
-        french_cities = es.search(index='test_grid_cities', body={'query': {'match': {'country_alpha2': 'fr'}}})
+        french_cities = es.search(index='test_grid_city', body={'query': {'match': {'country_alpha2': 'fr'}}})
         assert french_cities['hits']['total']['value'] == 629
-        paris = es.search(index='test_grid_cities', body={'query': {'percolate': {'field': 'query',
-                                                                                  'document': {'content': 'Paris'}}}})
+        paris = es.search(index='test_grid_city', body={'query': {'percolate': {'field': 'query',
+                                                                                'document': {'content': 'Paris'}}}})
         assert paris['hits']['total']['value'] == 3
-        es.delete_index(index='test_grid_cities')
-        es.delete_index(index='test_grid_institutions')
-        es.delete_index(index='test_grid_institutions_acronyms')
+        es.delete_index(index='test_grid_city')
+        es.delete_index(index='test_grid_institution')
+        es.delete_index(index='test_grid_institution_acronym')
