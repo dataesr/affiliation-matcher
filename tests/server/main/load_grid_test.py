25fd8aa2f4d244342b4f28f99f98e0ec31235468
eric <eric.jeangirard@recherche.gouv.fr>
2021-06-28 14:46:58 +0200

    [fix] change country matcher to fit the grid indexes format

--- tests/server/main/load_grid_test.py
+++ tests/server/main/load_grid_test.py
@@ -7,10 +7,8 @@ class TestLoadGrid:
         es = MyElastic()
         load_grid(index_prefix='test')
         french_cities = es.search(index='test_grid_city', body={'query': {'match': {'country_alpha2': 'fr'}}})
-        assert french_cities['hits']['total']['value'] == 629
+        assert 600 < french_cities['hits']['total']['value'] < 700
         paris = es.search(index='test_grid_city', body={'query': {'percolate': {'field': 'query',
                                                                                 'document': {'content': 'Paris'}}}})
-        assert paris['hits']['total']['value'] == 3
-        es.delete_index(index='test_grid_city')
-        es.delete_index(index='test_grid_institution')
-        es.delete_index(index='test_grid_institution_acronym')
+        assert len(paris['hits']['hits'][0]['_source']['country_alpha2']) == 3
+        es.delete_index(index='test_grid_*')
