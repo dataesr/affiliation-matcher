a9d169f2bab2b5ac2b1b7a1815652f6e27f37633
eric <eric.jeangirard@recherche.gouv.fr>
2021-06-29 14:31:54 +0200

    [feat] add country subdivisions from pycountry in country matcher

--- matcher/server/main/match_country.py
+++ matcher/server/main/match_country.py
@@ -5,15 +5,19 @@ DEFAULT_STRATEGIES = [
                 ['grid_city', 'grid_name', 'country_all_names'],
                 ['grid_city', 'grid_acronym', 'country_all_names'],
                 ['grid_city', 'country_all_names'],
+                ['grid_city', 'grid_name', 'country_subdivisions', 'country_alpha3'],
+                ['grid_city', 'grid_name', 'country_subdivisions'],
                 ['grid_city', 'grid_name', 'country_alpha3'],
-                ['grid_city', 'grid_acronym', 'country_alpha3'],
-                ['grid_city', 'country_alpha3'],
-                ['country_all_names']
+                ['grid_city', 'grid_acronym', 'country_subdivisions', 'country_alpha3'],
+                ['grid_city', 'country_subdivisions', 'country_alpha3'],
+                ['country_subdivisions', 'country_all_names'],
+                ['country_all_names'],
+                ['country_subdivisions', 'country_alpha3']
                 ]
 
 
-def match_country(query: str = '', strategies: list = None) -> dict:
+def match_country(query: str = '', strategies: list = None, index_prefix: str = '') -> dict:
     if strategies is None:
         strategies = DEFAULT_STRATEGIES
     matcher = Matcher()
-    return matcher.match(query=query, strategies=strategies, field='country_alpha2')
+    return matcher.match(query=query, strategies=strategies, field='country_alpha2', index_prefix=index_prefix)
