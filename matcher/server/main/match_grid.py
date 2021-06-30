e95a07aa4718bd26d1a1a377a219ff0b0862eecb
Anne L'Hôte <anne.lhote@gmail.com>
2021-06-30 09:12:25 +0200

    [feat] Create condition as dict in Matcher

--- matcher/server/main/match_grid.py
+++ matcher/server/main/match_grid.py
@@ -1,4 +1,4 @@
-from matcher.server.main.Matcher import Matcher
+from matcher.server.main.matcher import Matcher
 from matcher.server.main.utils import remove_ref_index
 
 
@@ -12,5 +12,6 @@ DEFAULT_STRATEGIES = [
 def match_grid(query: str = '', strategies: list = None, country_code: str = None) -> dict:
     if strategies is None:
         strategies = DEFAULT_STRATEGIES
+    condition = {'condition': 'grid_country', 'value': country_code}
     matcher = Matcher()
-    return matcher.match(query=query, strategies=strategies, year=country_code, pre_treatment_query=remove_ref_index)
+    return matcher.match(query=query, strategies=strategies, condition=condition, pre_treatment_query=remove_ref_index)
