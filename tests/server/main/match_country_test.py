5c646ea2a0be55fd2b3013975431c3260570ffc0
Anne L'Hôte <anne.lhote@gmail.com>
2021-07-21 12:33:26 +0200

    [ci] Add recall about the unit test on the country matcher

--- tests/server/main/match_country_test.py
+++ tests/server/main/match_country_test.py
@@ -45,4 +45,5 @@ class TestMatchCountry:
     
     def test_precision_recall(self, elasticsearch):
         precision_recall = compute_precision_recall(match_type='country', index_prefix='test')
-        assert precision_recall['precision'] >= 0.97
+        assert precision_recall['precision'] >= 0.99
+        assert precision_recall['recall'] >= 0.93
