203e182af5d15d82425fde54c8b5fe17b2500e74
Anne L'Hôte <anne.lhote@gmail.com>
2021-07-23 15:03:35 +0200

    [refactor] Clean code

--- matcher/server/main/load_country.py
+++ matcher/server/main/load_country.py
@@ -15,17 +15,17 @@ def download_country_data():
 
 
 def transform_country_data(raw_data):
-    subdivisions, subdivisions_code = {}, {}
-    for s in pycountry.subdivisions:
-        alpha2 = s.country_code.lower()
-        if alpha2 not in subdivisions:
-            subdivisions[alpha2] = []
-            subdivisions_code[alpha2] = []
-        subdivisions[alpha2].append(s.name)
+    subdivision_name, subdivision_code = {}, {}
+    for subdivision in pycountry.subdivisions:
+        alpha2 = subdivision.country_code.lower()
+        if alpha2 not in subdivision_name:
+            subdivision_name[alpha2] = []
+            subdivision_code[alpha2] = []
+        subdivision_name[alpha2].append(subdivision.name)
         if alpha2 == 'us':
-            subdivisions_code[alpha2].append(s.code[3:])
+            subdivision_code[alpha2].append(subdivision.code[3:])
         if alpha2 == 'gb':
-            subdivisions[alpha2].append('northern ireland')
+            subdivision_name[alpha2].append('northern ireland')
     countries = []
     for c in raw_data:
         # Alpha 2 - 3
@@ -34,36 +34,36 @@ def transform_country_data(raw_data):
         if alpha2 == 'gb':
             country['alpha3'].append('uk')
         # Names
-        all_names = []
+        names = []
         for field_name in ['name', 'official_name', 'common_name']:
             if field_name in c:
-                all_names.append(c[field_name])
+                names.append(c[field_name])
         if alpha2 == 'ru':
-            all_names.append('russia')
+            names.append('russia')
         if alpha2 == 'ci':
-            all_names.append('ivory coast')
+            names.append('ivory coast')
         if alpha2 == 'cv':
-            all_names.append('cape verde')
+            names.append('cape verde')
         if alpha2 == 'kp':
-            all_names.append('north korea')
+            names.append('north korea')
         if alpha2 == 'kr':
-            all_names.append('south korea')
+            names.append('south korea')
         if alpha2 == 'la':
-            all_names.append('laos')
+            names.append('laos')
         if alpha2 == 'sy':
-            all_names.append('syria')
+            names.append('syria')
         if alpha2 == 'tw':
-            all_names.append('taiwan')
+            names.append('taiwan')
         if alpha2 == 'vn':
-            all_names.append('vietnam')
-        all_names = list(set(all_names))
-        country['name'] = all_names
+            names.append('vietnam')
+        names = list(set(names))
+        country['name'] = names
         if 'name' in c:
             country['name'] = c['name']
         # Subdivisions
-        if alpha2 in subdivisions:
-            country['subdivision_name'] = list(set(subdivisions[alpha2]))
-            country['subdivision_code'] = list(set(subdivisions_code[alpha2]))
+        if alpha2 in subdivision_name:
+            country['subdivision_name'] = list(set(subdivision_name[alpha2]))
+            country['subdivision_code'] = list(set(subdivision_code[alpha2]))
         countries.append(country)
     return countries
 
