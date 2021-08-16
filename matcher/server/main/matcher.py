43889bb7a2a1f52b589dc8b44857ac0f6c9cafbe
Anne L'Hôte <anne.lhote@gmail.com>
2021-08-16 18:32:16 +0200

    [feat] Add equivalent_strategies as a new level to gather all equivalent strategies at the same level

--- matcher/server/main/matcher.py
+++ matcher/server/main/matcher.py
@@ -15,18 +15,22 @@ def identity(x: str = '') -> str:
 
 def filter_submatching_results(res: dict) -> dict:
     highlights = res['highlights']
+    logger.debug(highlights)
     logs = res['logs']
     results = res['results']
     if len(results) == 0:
         return res
     ids_to_remove = []
     matching_ids = list(highlights.keys())
+    logger.debug(matching_ids)
     all_id_combinations = itertools.combinations(matching_ids, 2)
-    criteria = highlights[matching_ids[0]].keys()
+    criteria_01 = highlights[matching_ids[0]].keys()
+    criteria_02 = highlights[matching_ids[1]].keys() if len(matching_ids) > 1 else []
+    criteria = list(set(list(criteria_01) + list(criteria_02)))
     for (id1, id2) in all_id_combinations:
         for criterion in criteria:
-            matching_elements_1 = BeautifulSoup(str(highlights[id1][criterion]), 'lxml').find_all('em')
-            matching_elements_2 = BeautifulSoup(str(highlights[id2][criterion]), 'lxml').find_all('em')
+            matching_elements_1 = BeautifulSoup(str(highlights[id1].get(criterion, '')), 'lxml').find_all('em')
+            matching_elements_2 = BeautifulSoup(str(highlights[id2].get(criterion, '')), 'lxml').find_all('em')
             if set(matching_elements_1) < set(matching_elements_2):
                 logs += f'<br> Removing {id1} as its {criterion} is included in the same for {id2}'
                 ids_to_remove.append(id1)
@@ -52,39 +56,53 @@ class Matcher:
         index_prefix = conditions.get('index_prefix', 'matcher')
         query = conditions.get('query', '')
         logs = f'<h1> &#128269; {query}</h1>'
-        for strategy in strategies:
-            strategy_results = None
+        for equivalent_strategies in strategies:
+            equivalent_strategies_results = None
             all_hits = {}
-            logs += f'<br/> - Matching strategy : {strategy}<br/>'
-            for criterion in strategy:
-                criterion_without_source = '_'.join(criterion.split('_')[1:])
-                if criterion_without_source in conditions:
-                    criterion_query = conditions[criterion_without_source]
-                else:
-                    criterion_query = pre_treatment_query(query)
-                body = {'query': {'percolate': {'field': 'query', 'document': {'content': criterion_query}}},
+            logs += f'<br/> - Matching equivalent strategies : {equivalent_strategies}<br/>'
+            for strategy in equivalent_strategies:
+                strategy_results = None
+                for criterion in strategy:
+                    # TODO : remove index_prefix
+                    criterion_without_source = '_'.join(criterion.split('_')[1:])
+                    if criterion_without_source in conditions:
+                        criterion_query = conditions[criterion_without_source]
+                    else:
+                        criterion_query = pre_treatment_query(query)
+                    body = {
+                        'query': {'percolate': {'field': 'query', 'document': {'content': criterion_query}}},
                         '_source': {'includes': [field]},
-                        'highlight': {'fields': {'content': {'type': 'unified'}}}}
-                index = get_index_name(index_name=criterion, source='', index_prefix=index_prefix)
-                hits = self.es.search(index=index, body=body).get('hits', []).get('hits', [])
-                all_hits[criterion] = hits
-                criteria_results = [hit.get('_source', {}).get(field) for hit in hits]
-                criteria_results = [item for sublist in criteria_results for item in sublist]
-                criteria_results = list(set(criteria_results))
-                if strategy_results is None:
-                    strategy_results = criteria_results
+                        'highlight': {'fields': {'content': {'type': 'unified'}}}
+                    }
+                    index = get_index_name(index_name=criterion, source='', index_prefix=index_prefix)
+                    hits = self.es.search(index=index, body=body).get('hits', []).get('hits', [])
+                    all_hits[criterion] = hits
+                    criteria_results = [hit.get('_source', {}).get(field) for hit in hits]
+                    criteria_results = [item for sublist in criteria_results for item in sublist]
+                    criteria_results = list(set(criteria_results))
+                    if strategy_results is None:
+                        strategy_results = criteria_results
+                    else:
+                        # Intersection
+                        strategy_results = [result for result in strategy_results if result in criteria_results]
+                    logs += f'Criteria : {criterion} : {len(criteria_results)} matches <br/>'
+                if equivalent_strategies_results is None:
+                    equivalent_strategies_results = strategy_results
                 else:
-                    # Intersection
-                    strategy_results = [result for result in strategy_results if result in criteria_results]
-                logs += f'Criteria : {criterion} : {len(criteria_results)} matches <br/>'
-            logs += f'Strategy has {len(strategy_results)} possibilities that match all criteria<br/>'
-            # Strategies stopped as soon as a first result is met
+                    # Union
+                    equivalent_strategies_results += strategy_results
+                    # Remove duplicates
+                    equivalent_strategies_results = list(set(equivalent_strategies_results))
+                logs += f'Strategy : {strategy} : {len(strategy_results)} matches <br/>'
+                logs += f'Equivalent strategies has {len(equivalent_strategies_results)} possibilities that match ' \
+                        f'one of the strategy<br/>'
+            # Strategies stopped as soon as a first result is met for an equivalent_strategies
             all_highlights = {}
-            if len(strategy_results) > 0:
-                logs += f'<hr>Results: {strategy_results}'
+            if len(equivalent_strategies_results) > 0:
+                logs += f'<hr>Results: {equivalent_strategies_results}'
                 for matching_criteria in all_hits:
                     for hit in all_hits[matching_criteria]:
-                        matching_ids = list(set(hit['_source'][field]) & set(strategy_results))
+                        matching_ids = list(set(hit['_source'][field]) & set(equivalent_strategies_results))
                         for matching_id in matching_ids:
                             if matching_id not in all_highlights:
                                 all_highlights[matching_id] = {}
@@ -93,7 +111,7 @@ class Matcher:
                             current_highlight = hit.get('highlight', {}).get('content', [])
                             if current_highlight not in all_highlights[matching_id][matching_criteria]:
                                 all_highlights[matching_id][matching_criteria].append(current_highlight)
-                final_res = {'results': strategy_results, 'highlights': all_highlights, 'logs': logs}
+                final_res = {'results': equivalent_strategies_results, 'highlights': all_highlights, 'logs': logs}
                 final_res = filter_submatching_results(final_res)
                 logs = final_res['logs']
                 for matching_id in final_res['highlights']:
