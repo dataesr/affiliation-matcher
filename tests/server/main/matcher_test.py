0faa1625ead2afb19da3f9a7a4fc47018f128008
Anne L'Hôte <anne.lhote@gmail.com>
2021-08-17 17:55:42 +0200

    [feat] Improve submatching rules

--- tests/server/main/matcher_test.py
+++ tests/server/main/matcher_test.py
@@ -6,10 +6,19 @@ from matcher.server.main.matcher import filter_submatching_results
 class TestMatcher:
     @pytest.mark.parametrize(
         'highlights,results,expected_results', [
+            ({'us': {'grid_name': ['<em>Cambridge University</em>', '<em>Medical Cambridge University</em>']},
+              'pt': {'grid_name': ['<em>Cambridge University</em>']}},
+             ['us', 'pt'], ['us']),
+            ({'us': {'grid_name': ['<em>Cambridge University</em>']},
+              'pt': {'grid_name': ['<em>Cambridge University</em>', '<em>Medical Cambridge University</em>']}},
+             ['us', 'pt'], ['pt']),
+            ({'us': {'grid_name': ['<em>Cambridge University</em>', '<em>Medical Cambridge University</em>']},
+              'pt': {'grid_name': ['<em>Cambridge University</em>', '<em>University of Porto</em>']}},
+             ['us', 'pt'], ['us', 'pt']),
             ({'us': {'grid_city': ['<em>Paris</em>', '<em>Philadelphia</em>']},
-              'fr': {'grid_city': ['<em>Paris</em>']}}, ['us', 'fr'], ['us']),
-            ({'us': {'grid_name': ['<em>Cambridge Public School</em>', '<em>Cambridge School</em>']},
-              'pt': {'grid_name': ['<em>Cambridge School</em>']}}, ['us', 'pt'], ['us'])
+              'fr': {'grid_city': ['<em>Paris</em>']}}, ['us', 'fr'], ['us', 'fr']),
+            ({'us': {'grid_city': ['<em>Paris</em>']},
+              'fr': {'grid_city': ['<em>Paris</em>', '<em>Philadelphia</em>']}}, ['us', 'fr'], ['us', 'fr'])
         ])
     def test_filter_submatching_results(self, highlights, results, expected_results) -> None:
         res = {'highlights': highlights, 'logs': '', 'results': results}
