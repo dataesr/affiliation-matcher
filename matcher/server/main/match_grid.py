36933e63bdddd55e3aff0ad2db1d76bd38aaff3d
Anne L'Hôte <anne.lhote@gmail.com>
2021-09-07 10:03:08 +0200

    [refactor] Simplify DEFAULT_STRATEGY for matchers

--- matcher/server/main/match_grid.py
+++ matcher/server/main/match_grid.py
@@ -1,7 +1,6 @@
 from matcher.server.main.matcher import Matcher
 from matcher.server.main.utils import remove_ref_index
 
-
 DEFAULT_STRATEGIES = [
     [['grid_name', 'grid_acronym', 'grid_city', 'grid_country'],
      ['grid_name', 'grid_acronym', 'grid_city', 'grid_country_code']],
@@ -13,8 +12,6 @@ DEFAULT_STRATEGIES = [
 
 
 def match_grid(conditions: dict) -> dict:
-    strategies = conditions.get('strategies')
-    if strategies is None:
-        strategies = DEFAULT_STRATEGIES
+    strategies = conditions.get('strategies', DEFAULT_STRATEGIES)
     matcher = Matcher()
     return matcher.match(conditions=conditions, strategies=strategies, pre_treatment_query=remove_ref_index)
