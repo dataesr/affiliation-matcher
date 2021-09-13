5e89d9a8a0dc1f4fcfca70fa49a947a0e52c150c
Anne L'Hôte <anne.lhote@gmail.com>
2021-09-13 11:49:28 +0200

    [feat][grid] Improve precision by removing parent institutes from results

--- matcher/server/main/match_grid.py
+++ matcher/server/main/match_grid.py
@@ -11,10 +11,31 @@ DEFAULT_STRATEGIES = [
 STOPWORDS_STRATEGIES = {'grid_name': ENGLISH_STOP}
 
 
+def get_ancestors(grid: str, es, index_prefix: str) -> list:
+    index = f'{index_prefix}_grid_parent'
+    body = {'query': {'query_string': {'query': grid}}, '_source': {'includes': ['query']}}
+    hits = es.search(index=index, body=body).get('hits', []).get('hits', [])
+    parents = [hit.get('_source', {}).get('query', {}).get('match_phrase', {}).get('content', {}).get('query')
+               for hit in hits]
+    ancestors = parents
+    for parent in parents:
+        ancestors += get_ancestors(grid=parent, es=es, index_prefix=index_prefix)
+    ancestors = list(set(ancestors))
+    return ancestors
+
+
+def remove_ancestors(grids: list, es, index_prefix: str) -> list:
+    grids_copy = grids.copy()
+    for grid in grids:
+        ancestors = get_ancestors(grid=grid, es=es, index_prefix=index_prefix)
+        grids_copy = list(set(grids_copy) - set(ancestors))
+    return grids_copy
+
+
 def match_grid(conditions: dict) -> dict:
     strategies = conditions.get('strategies')
     if strategies is None:
         strategies = DEFAULT_STRATEGIES
     matcher = Matcher()
     return matcher.match(conditions=conditions, strategies=strategies, pre_treatment_query=remove_ref_index,
-                         stopwords_strategies=STOPWORDS_STRATEGIES)
+                         stopwords_strategies=STOPWORDS_STRATEGIES, post_treatment_results=remove_ancestors)
