2889d8e185f020d47b04145626b201619862e136
Anne L'Hôte <anne.lhote@gmail.com>
2022-07-01 11:56:19 +0200

    [fix] Set GRID_DUMP_URL as last available dump, close #54

--- project/server/main/config.py
+++ project/server/main/config.py
@@ -1,22 +1,9 @@
 import os
 import requests
 
-from bs4 import BeautifulSoup
-
 from project.server.main.logger import get_logger
 
 logger = get_logger(__name__)
-
-
-def get_last_grid_dump_url():
-    try:
-        soup = BeautifulSoup(requests.get(GRID_URL).text, 'lxml')
-        grid_dump_url = soup.find('a', class_='btn-red')['href'].strip()
-        logger.debug(f'Last grid dump url found: {grid_dump_url}')
-    except:
-        grid_dump_url = 'https://ndownloader.figshare.com/files/30895309'
-        logger.error(f'Grid dump url detection failed, using {grid_dump_url} instead')
-    return grid_dump_url
 
 
 def get_last_ror_dump_url():
@@ -26,7 +13,8 @@ def get_last_ror_dump_url():
         ror_dump_url = response['hits']['hits'][0]['files'][0]['links']['self']
         logger.debug(f'Last ROR dump url found: {ror_dump_url}')
     except:
-        ror_dump_url = 'https://zenodo.org/api/files/b3deada8-9485-45f9-a6e5-50c2a512b12d/v1.0-2022-03-17-ror-data.json.zip'
+        ror_dump_url = 'https://zenodo.org/api/files/b3deada8-9485-45f9-a6e5-50c2a512b12d/' \
+            'v1.0-2022-03-17-ror-data.json.zip'
         logger.error(f'ROR dump url detection failed, using {ror_dump_url} instead')
     return ror_dump_url
 
@@ -42,11 +30,10 @@ ELASTICSEARCH_PORT = '9200'
 ELASTICSEARCH_LOGIN = None
 ELASTICSEARCH_PASSWORD = None
 
-GRID_URL = 'https://www.grid.ac/downloads'
+GRID_DUMP_URL = 'https://digitalscience.figshare.com/ndownloader/files/30895309'
 SCANR_DUMP_URL = 'https://storage.gra.cloud.ovh.net/v1/AUTH_32c5d10cb0fe4519b957064a111717e3/scanR/organizations.json'
 ZONE_EMPLOI_INSEE_DUMP = 'https://www.insee.fr/fr/statistiques/fichier/4652957/ZE2020_au_01-01-2021.zip'
 
-GRID_DUMP_URL = get_last_grid_dump_url()
 ROR_DUMP_URL = get_last_ror_dump_url()
 
 
@@ -60,4 +47,4 @@ elif APP_ENV == 'production':
     ELASTICSEARCH_LOGIN = os.getenv('ES_LOGIN_MATCHER')
     ELASTICSEARCH_PASSWORD = os.getenv('ES_PASSWORD_MATCHER')
 else:
-    ELASTICSEARCH_URL = ELASTICSEARCH_HOST + ':' + ELASTICSEARCH_PORT
+    ELASTICSEARCH_URL = f'{ELASTICSEARCH_HOST}:{ELASTICSEARCH_PORT}'
