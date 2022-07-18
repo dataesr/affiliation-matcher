61005b07805a906187ef0a1993a91710619c54e3
Anne L'Hôte <anne.lhote@gmail.com>
2022-07-18 15:18:35 +0200

    [feat] Release new Docker image of es_icu with Elasticsearch 7.12.0

--- docker_push_ext.sh
+++ docker_push_ext.sh
@@ -1,7 +1,9 @@
-sudo docker build -f Dockerfile_es -t dataesr/es_icu -t dataesr/es_icu:1.0 .
-sudo docker push dataesr/es_icu
+sudo docker build -f Dockerfile_es -t dataesr/es_icu:latest -t dataesr/es_icu:7.12.0 .
+sudo docker push dataesr/es_icu:latest
+sudo docker push dataesr/es_icu:7.12.0
 
-sudo docker build -f Dockerfile -t dataesr/affiliation-matcher -t dataesr/affiliation-matcher:0.1.3 .
-sudo docker push dataesr/affiliation-matcher
+sudo docker build -f Dockerfile -t dataesr/affiliation-matcher:latest -t dataesr/affiliation-matcher:0.1.3 .
+sudo docker push dataesr/affiliation-matcher:latest
+sudo docker push dataesr/affiliation-matcher:0.1.3
 
 # docker-compose down && docker-compose pull && docker-compose up
