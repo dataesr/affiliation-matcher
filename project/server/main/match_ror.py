800caf9e7bf935a1242246480e49c7bf8e153829
eric <eric.jeangirard@recherche.gouv.fr>
2022-08-24 16:02:35 +0200

    abbrevations for ror matcher

--- project/server/main/match_ror.py
+++ project/server/main/match_ror.py
@@ -1,3 +1,4 @@
+import re
 from project.server.main.matcher import Matcher
 from project.server.main.utils import ENGLISH_STOP, FRENCH_STOP, remove_ref_index
 
@@ -10,10 +11,36 @@ DEFAULT_STRATEGIES = [
     [['ror_acronym', 'ror_city', 'ror_country'], ['ror_acronym', 'ror_city', 'ror_country_code']],
     [['ror_name', 'ror_acronym', 'ror_city'], ['ror_name', 'ror_acronym', 'ror_country'],
      ['ror_name', 'ror_acronym', 'ror_country_code']],
+    [['ror_name', 'ror_country']],
     [['ror_name', 'ror_city']]
+#    ,[['ror_name_unique', 'ror_city']],
+#    [['ror_acronym_unique', 'ror_city']],
+#    [['ror_name_unique', 'ror_country']],
+#    [['ror_acronym_unique', 'ror_country']]
 ]
 STOPWORDS_STRATEGIES = {'ror_name': ENGLISH_STOP + FRENCH_STOP}
 
+def replace_synonym(query, source, target):
+    rgx = re.compile("(?i)(" + source + ")( |,)")
+    return rgx.sub(target+" ", query)
+
+# Done here rather than in synonym settings in ES as they seem to cause highlight bugs
+def pre_treatment_ror(query: str = '') -> str:
+    query = remove_ref_index(query)
+    # If query starts with a digit that can be a reference index
+    synonyms = [
+            ['comput.', 'computer'],
+            ['dpt.', 'department'],
+            ['eng.', 'engineering'],
+            ['inst.', 'institute'],
+            ['mech.', 'mechanics'],
+            ['sci.', 'sciences'],
+            ['technol.', 'technology'],
+            ['univ.', 'university']
+            ]
+    for synonym in synonyms:
+        query = replace_synonym(query, synonym[0], synonym[1])
+    return query.lower()
 
 def match_ror(conditions: dict) -> dict:
     strategies = conditions.get('strategies')
@@ -24,6 +51,6 @@ def match_ror(conditions: dict) -> dict:
         field='rors',
         conditions=conditions,
         strategies=strategies,
-        pre_treatment_query=remove_ref_index,
+        pre_treatment_query=pre_treatment_ror,
         stopwords_strategies=STOPWORDS_STRATEGIES,
     )
