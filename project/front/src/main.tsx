d743383b826d39a1fe5fa4aeaeccf8f2d126ff61
victor barbier <victor.barbier@recherche.gouv.fr>
2024-08-28 13:35:39 +0200

    feat(dsfr): add config in main

--- project/front/src/main.tsx
+++ project/front/src/main.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { DSFRConfig } from "@dataesr/dsfr-plus"
 import { BrowserRouter } from 'react-router-dom';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
@@ -9,13 +10,15 @@ import './styles/index.scss';
 
 const queryClient = new QueryClient();
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <QueryClientProvider client={queryClient}>
-        <ReactQueryDevtools />
-        <Router />
-      </QueryClientProvider>
-    </BrowserRouter>
-  </React.StrictMode>,
-);
+    <DSFRConfig>
+      <BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+          <ReactQueryDevtools />
+          <Router />
+        </QueryClientProvider>
+      </BrowserRouter>
+    </DSFRConfig>
+  </React.StrictMode>
+)
